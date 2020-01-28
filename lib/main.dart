8699abbd6540125cdefea6ee50c81800dd449995
darshan7patel <dobariyadarshan9@gmail.com>
2020-01-28 18:24:45 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+void main() async { 
+  List currencies = await getCurrencies();
+  print(currencies);
+  runApp(new MyApp(currencies));
+}
+
+class MyApp extends StatelessWidget {
+  final List _currencies;
+  MyApp(this._currencies);
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme : new ThemeData(primarySwatch: Colors.purple),
+      home: new HomePage(_currencies),
+    );
+  }
+}
+
+Future<List> getCurrencies() async{
+    String cryptoUrl = "https://api.coinmarketcap.com/v1/ticker/?limit=50";
+    http.Response response = await http.get(cryptoUrl);
+    return json.decode(response.body);
+  }
